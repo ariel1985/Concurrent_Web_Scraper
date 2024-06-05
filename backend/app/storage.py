5d217dbad4f4efda86c22762ff685be41b37cd85
ariel1985 <rothemariel@gmail.com>
2024-06-05 16:03:04 +0300

    Basic concurrent scraper independant folder

---
+++ backend/app/storage.py
@@ -0,0 +1,12 @@
+import json
+from datetime import datetime
+
+def save_to_json(data):
+    
+    filepath = 'app/scraped_data/'  # save data in 'data' folder
+    
+    # add current datetime to filename format 'data_YYYY-MM-DD-HH-MM-SS.json'
+    filename = f'{filepath}data_{datetime.now().strftime("%Y-%m-%d-%H-%M-%S")}.json'
+    
+    with open(filename, 'w') as f:
+        json.dump(data, f, indent=4)
--- storage.py
+++
@@ -1,5 +0,0 @@
-import json
-
-def save_to_json(data, filename='data.json'):
-    with open(filename, 'w') as f:
-        json.dump(data, f, indent=4)
